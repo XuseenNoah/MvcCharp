33dbec689bed54ccaa23da94851f58da1132b2f0
xuseen muse <xuseens@live.com>
2019-02-20 11:37:53 +0300

    first commit

---
+++ MvcCsharp/Controllers/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCsharp.Controllers
+{
+    public class MainController :Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       public ActionResult Search(string name)
+        {
+            var input = Server.HtmlDecode(name);
+            return Content(input);
+        }
+
+        //public ActionResult EmptyResutl()
+        //{
+        //    //return EmptyResutl();
+        //    return PartialView();
+        //    return Json();
+        //    return JavaScript();
+        //}
+    }
+}
